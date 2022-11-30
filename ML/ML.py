999af19c6df9c9b35637f84e93d1683036b80aa9
Nathan BLISCAUX <119364249+nathanbliscaux@users.noreply.github.com>
2022-11-30 14:40:39 +0100

    Add files via upload

---
+++ ML/ML.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 30 13:38:16 2022
+
+@author: nathan.bliscaux
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib as mlp
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+
+
+plt.rcParams["figure.figsize"]=[3,3]
